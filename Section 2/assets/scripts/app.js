e8ce67b500c2bd32f2751a17180a9a194d4eee2e
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2021-08-30 21:23:09 -0500

    Section 2 & 3
    - Basics: Variables , Data Types, Operators & Functions
    - Efficient Development & Debugging

--- Section 2/assets/scripts/app.js
+++ Section 2/assets/scripts/app.js
@@ -1,1 +1,65 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult; 
+let logEntries = [];
+
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}//End of getUserNumberInput
+
+function createAndWriteOperator(operator, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult){
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}//End of writeToLog
+
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber; //+ parseInt(x) = + +x
+    createAndWriteOperator('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}//End of add 
+
+
+function substract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOperator('-', initialResult, enteredNumber);
+    writeToLog('SUBSTRACT', initialResult, enteredNumber, currentResult);
+}//End of substract
+
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOperator('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}//End of multiply
+
+
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOperator('/', initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}//End of divide
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
